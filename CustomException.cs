710e3f414bb91f0808f5195ea4c22d14c5f32877
Diamantine <meteoritus75@gamil.com>
2024-06-11 10:42:45 +0200

    Challenge les Exceptions

---
+++ CustomException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LES_EXCEPTIONS
+{
+    public class CustomException : Exception
+    {
+        public DateTime ErrorTime { get; set; }
+
+        public CustomException(string message)
+            : base(message)
+        {
+            ErrorTime = DateTime.Now;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace LES_EXCEPTIONS
+{
+    public class Program
+    {
+        static void ThrowError()
+        {
+            throw new CustomException("Une erreur personnalisée s'est produite.");
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                ThrowError();
+            }
+            catch (CustomException e)
+            {
+                Console.WriteLine($"Message d'erreur' : {e.Message}");
+                Console.WriteLine($"Heure de l'erreur' : {e.ErrorTime}");
+            }
+        }
+    }
+}
